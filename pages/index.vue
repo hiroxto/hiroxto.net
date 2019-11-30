5d17f8cd9ab31fe62eb53942f0f424b8e324f877
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2019-11-30 23:21:43 +0900

    Remove flex class from container

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container flex min-h-screen text-left">
+  <div class="container min-h-screen text-left">
     <div class="pt-8">
       <h1 class="title block text-6xl text-gray-800">
         hiroto-k.net
