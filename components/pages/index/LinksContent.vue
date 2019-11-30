a7aeb203395dfd194623af7fd282ca4cc0c87d7c
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2019-11-30 22:34:50 +0900

    Create LinksContent component

---
+++ components/pages/index/LinksContent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'LinksContent',
+});
+</script>
+
+<style scoped>
+
+</style>
