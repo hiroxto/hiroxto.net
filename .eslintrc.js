1566e06109699ae4f5e102aec0110c670e506ee0
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2019-11-30 20:55:14 +0900

    Update eslint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,45 @@
 module.exports = {
   root: true,
+  parser: '@typescript-eslint/parser',
   env: {
     browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
+    node: true,
   },
   extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/strongly-recommended',
+    'plugin:promise/recommended',
+    'standard',
   ],
-  // add your custom rules here
+  plugins: [
+    '@typescript-eslint',
+    'vue',
+    'promise',
+  ],
   rules: {
-  }
-}
+    'comma-dangle': ['error', 'always-multiline'],
+    semi: ['error', 'always'],
+    'no-console': 'off',
+    'no-new': 'off',
+    indent: ['error', 2],
+    'quote-props': ['error', 'as-needed'],
+    'vue/script-indent': ['error', 2, { baseIndent: 0, switchCase: 1 }],
+    'vue/html-indent': ['error', 2, { attribute: 1, closeBracket: 0, baseIndent: 1 }],
+    'vue/html-closing-bracket-newline': ['error', { singleline: 'never', multiline: 'always' }],
+    'vue/component-name-in-template-casing': ['error', 'kebab-case'],
+    'vue/html-self-closing': ['error', { html: { void: 'never', normal: 'never', component: 'never' } }],
+    'vue/max-attributes-per-line': ['error', { singleline: 2 }],
+    'vue/this-in-template': ['error'],
+    'promise/catch-or-return': ['error', { allowThen: true, terminationMethod: ['catch', 'asCallback', 'finally'] }],
+    '@typescript-eslint/array-type': ['error', { default: 'array' }],
+    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+  },
+  overrides: [
+    {
+      files: ['*.vue'],
+      rules: {
+        indent: 'off',
+      },
+    },
+  ],
+};
