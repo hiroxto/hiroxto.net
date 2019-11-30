772612fda4233dd8046e27f5a76d400b49c6156f
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2019-11-30 23:32:06 +0900

    Update LinksContent component

--- components/pages/index/LinksContent.vue
+++ components/pages/index/LinksContent.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>
+  <div class="bg-white md:flex py-6 px-6">
+    <div class="md:flex-shrink-0">
+      <div class="text-lg text-purple-600 font-bold uppercase">
+        Links
+      </div>
+    </div>
   </div>
 </template>
 
