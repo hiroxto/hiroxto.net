8e5b3fe397dfb11a4d14f59e9f559d6f30328626
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2019-12-01 00:25:21 +0900

    Create Separator component

---
+++ components/ui/Separator.vue
@@ -0,0 +1,26 @@
+<template>
+  <div :class="className">
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropOptions } from 'vue';
+
+export default Vue.extend({
+  name: 'Separator',
+  props: {
+    size: {
+      type: String,
+      required: false,
+      default () {
+        return '5';
+      },
+    } as PropOptions<string>,
+  },
+  computed: {
+    className (): string {
+      return `py-${this.size}`;
+    },
+  },
+});
+</script>
