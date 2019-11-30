10170e630374eda388c2ab67d22fe137ce0dd35f
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2019-12-01 00:05:06 +0900

    Create sub domain links list

--- components/pages/index/LinksContent.vue
+++ components/pages/index/LinksContent.vue
@@ -10,6 +10,16 @@
       </h4>
 
       <ul class="list-disc">
+        <li
+          v-for="(subDomainLink, subDomainLinksKey) in subDomainLinks"
+          :key="subDomainLinksKey"
+        >
+          <a
+            v-text="subDomainLink.name"
+            :href="subDomainLink.to"
+          >
+          </a>
+        </li>
       </ul>
     </div>
   </div>
@@ -17,9 +27,24 @@
 
 <script lang="ts">
 import Vue from 'vue';
+import { Link } from '~/types';
 
 export default Vue.extend({
   name: 'LinksContent',
+  computed: {
+    subDomainLinks (): Link[] {
+      return [
+        {
+          name: 'utils.hiroto-k.net',
+          to: 'https://utils.hiroto-k.net/',
+        },
+        {
+          name: 'train-photo-blog.hiroto-k.net',
+          to: 'https://train-photo-blog.hiroto-k.net/',
+        },
+      ];
+    },
+  },
 });
 </script>
 
