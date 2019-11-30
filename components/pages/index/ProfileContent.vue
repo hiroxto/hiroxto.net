c7570d5bcd3c69f1ffc21408ab3666d86a3ccc9c
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2019-11-30 23:32:56 +0900

    Update ProfileContent component

--- components/pages/index/ProfileContent.vue
+++ components/pages/index/ProfileContent.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="bg-white md:flex py-6 px-6">
     <div class="md:flex-shrink-0">
+      <div class="text-lg text-purple-600 font-bold uppercase pb-4">
+        Profile
+      </div>
+
       <img
         class="rounded-full h-20 w-20"
         src="https://avatars2.githubusercontent.com/u/12016555?s=460&v=4"
@@ -8,16 +12,13 @@
       >
     </div>
 
-    <div class="mt-4 md:mt-0 md:ml-6">
-      <div class="text-lg text-purple-600 font-bold">
-        Profile
-      </div>
-
-      <p class="text-lg text-gray-900 font-bold block mt-1 leading-tight">
+    <div class="mt-4 md:mt-0 md:ml-6 pt-6">
+      <p class="text-lg text-gray-900 font-bold block mt-2 mb-2 leading-tight">
         hiroto-k / hiroxto
       </p>
 
-      <p class="mt-2 text-gray-600">
+      <p class="text-gray-600">
+        Description
       </p>
     </div>
   </div>
