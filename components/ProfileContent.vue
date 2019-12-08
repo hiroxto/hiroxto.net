66217e637caff0d1b1ca395d213658010b12f3ee
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2019-12-08 23:06:34 +0900

    Create the profile page contents

--- components/ProfileContent.vue
+++ components/ProfileContent.vue
@@ -13,22 +13,108 @@
     </div>
 
     <div class="mt-4 md:mt-0 md:ml-6">
-      <p class="text-xl text-gray-900 font-bold pb-4 font-source-sans-pro">
-        hiroto-k / hiroxto
+      <p class="text-2xl text-gray-900 font-bold pb-4 font-source-sans-pro" v-text="names">
       </p>
 
-      <p class="text-gray-600">
-        Description
-      </p>
+      <profile-item title="Likes" :single-text="true">
+        {{ likes }}
+      </profile-item>
+
+      <profile-item title="プログラミング">
+        <p class="profile-item-text" v-text="programming">
+        </p>
+        <p class="profile-item-text">
+          普段はPHPとRubyを書きつつ、JavaScript/TypeScriptを使ったVue.jsやNuxt.jsも書きます。
+        </p>
+        <p class="profile-item-text">
+          エディタはVSCodeとJetBrains系のIDEを使ってます。
+        </p>
+      </profile-item>
+
+      <profile-item title="アニメ" :single-text="true">
+        適当に色々と見ています。
+      </profile-item>
+
+      <profile-item title="声優">
+        <p class="profile-item-text">
+          言うほど声優オタクではありませんが、主に以下の方々が好きです。
+        </p>
+
+        <ul class="list-disc ml-5">
+          <li
+            v-for="(voiceActorName, voiceActorNamesKey) in voiceActorNames"
+            :key="voiceActorNamesKey"
+            v-text="voiceActorName"
+            class="profile-item-text leading-normal"
+          >
+          </li>
+        </ul>
+      </profile-item>
+
+      <profile-item title="ゲーム" :single-text="true">
+        8 beat Story♪とCUE!だけはまともにやっています。リステは曲だけ追っかけ。
+      </profile-item>
+
+      <profile-item title="写真" :single-text="true">
+        たまに鉄道写真や風景写真を撮ります。電車おたくは他界気味なので最近はあまり撮らず。撮りたい被写体を適当に撮っています。
+      </profile-item>
+
+      <profile-item title="ラーメン" :single-text="true">
+        4日間連続で昼夜に食べられるくらいには好きです。(朝からラーメンとか、5日連続ラーメンとかは普通に嫌。)
+      </profile-item>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import ProfileItem from './ProfileItem';
 
 export default Vue.extend({
   name: 'ProfileContent',
+  components: {
+    ProfileItem,
+  },
+  computed: {
+    names (): string {
+      return 'hiroto-k / hiroxto';
+    },
+    joinSeparator (): string {
+      return ', ';
+    },
+    likes (): string {
+      return [
+        'プログラミング',
+        'アニメ',
+        '声優',
+        'ゲーム',
+        '写真',
+        'ラーメン',
+        'etc...',
+      ].join(this.joinSeparator);
+    },
+    programming (): string {
+      return [
+        'PHP',
+        'Ruby',
+        'JavaScript',
+        'Laravel',
+        'Vue.js',
+        'Nuxt.js',
+      ].join(this.joinSeparator);
+    },
+    voiceActorNames (): string[] {
+      return [
+        '大橋彩香',
+        '安齋由香里',
+        '小澤亜李',
+        '嶺内ともみ',
+        '和氣あず未',
+      ].map((name: string): string => {
+        return `${name}さん`;
+      });
+    },
+  },
 });
 </script>
 
---
+++ components/ProfileItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="profile-item">
+    <h4 class="profile-item-title" v-text="title">
+    </h4>
+
+    <template v-if="singleText">
+      <p class="profile-item-text">
+        <slot>
+        </slot>
+      </p>
+    </template>
+    <template v-else>
+      <slot>
+      </slot>
+    </template>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropOptions } from 'vue';
+
+export default Vue.extend({
+  name: 'ProfileItem',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    } as PropOptions<string>,
+    singleText: {
+      type: Boolean,
+      default (): boolean {
+        return false;
+      },
+    } as PropOptions<boolean>,
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
