f09f5e1c3421b201eddd1507eb943a128023ad47
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2019-12-08 21:53:26 +0900

    Use Source Sans Pro font in page and content titles

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="container min-h-screen text-left">
     <div class="py-8">
-      <h1 class="title text-gray-800 xl:text-6xl lg:text-6xl md:text-6xl sm:text-5xl text-5xl block">
+      <h1 class="title text-gray-800 xl:text-6xl lg:text-6xl md:text-6xl sm:text-5xl text-5xl block font-source-sans-pro">
         hiroto-k.net
       </h1>
 
-      <h2 class="subtitle text-gray-800 xl:text-3xl lg:text-3xl md:text-3xl sm:text-2xl text-2xl pb-8">
+      <h2 class="subtitle text-gray-800 xl:text-3xl lg:text-3xl md:text-3xl sm:text-2xl text-2xl pb-8 font-source-sans-pro">
         Home page of hiroto-k.
       </h2>
 
