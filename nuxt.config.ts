a3eee7a9e20788c190303ea27267bfef334f40ea
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2019-12-08 23:21:31 +0900

    Fix default heads

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,11 +7,11 @@ const config: Configuration = {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    titleTemplate: '%s | hiroto-k.net',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { hid: 'description', name: 'description', content: 'Generic description.' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
