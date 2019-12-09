19117340a3e0cbfb5303a368d2d29cd77d147ddf
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2019-12-09 20:28:43 +0900

    Fix className implementation
    This change is take measures to Purgecss CSS removal.

--- components/Separator.vue
+++ components/Separator.vue
@@ -19,7 +19,30 @@ export default Vue.extend({
   },
   computed: {
     className (): string {
-      return `py-${this.size}`;
+      const pyClassName = `py-${this.size}`;
+      const allowClasses = [
+        'py-0',
+        'py-1',
+        'py-2',
+        'py-3',
+        'py-4',
+        'py-5',
+        'py-6',
+        'py-8',
+        'py-10',
+        'py-12',
+        'py-16',
+        'py-20',
+        'py-24',
+        'py-32',
+        'py-40',
+        'py-48',
+        'py-56',
+        'py-64',
+        'py-px',
+      ];
+
+      return allowClasses.includes(pyClassName) ? pyClassName : 'py-5';
     },
   },
 });
