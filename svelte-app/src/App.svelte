595fe86bcd770405722d60f1802f2aea30ff5571
Hiroto Kitazawa <hiroxto@gmail.com>
2023-06-01 22:48:25 +0900

    Stylelintでスタイルを修正

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -9,20 +9,20 @@
 
 <style>
     main {
-        text-align: center;
-        padding: 1em;
         max-width: 240px;
+        padding: 1em;
         margin: 0 auto;
+        text-align: center;
     }
 
     h1 {
-        color: #ff3e00;
-        text-transform: uppercase;
         font-size: 4em;
         font-weight: 100;
+        color: #ff3e00;
+        text-transform: uppercase;
     }
 
-    @media (min-width: 640px) {
+    @media (width >= 640px) {
         main {
             max-width: none;
         }
