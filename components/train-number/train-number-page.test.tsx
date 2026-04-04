c7e12d947ccbbdf450e1e1b166c8cb75d18b9379
Hiroto Kitazawa <hiroxto@gmail.com>
2026-04-05 01:13:20 +0900

    chore: ページ表示テストを軽量化

--- components/train-number/train-number-page.test.tsx
+++ components/train-number/train-number-page.test.tsx
@@ -4,7 +4,7 @@ import { renderWithMantine } from '@/test/test-utils';
 import { TrainNumberPage } from './train-number-page';
 
 describe('TrainNumberPage', () => {
-    it('markdown を HTML として表示し見出しIDも付与すること', () => {
+    it('markdown を HTML として表示すること', () => {
         const { container } = renderWithMantine(
             <TrainNumberPage
                 title="2018年3月17日 改正"
@@ -22,12 +22,9 @@ describe('TrainNumberPage', () => {
             />,
         );
 
-        expect(screen.getAllByRole('heading', { name: '2018年3月17日 改正' })).toHaveLength(2);
         expect(screen.getByRole('link', { name: 'ツール一覧' })).toHaveAttribute('href', '/tools');
         expect(screen.getByRole('link', { name: '列車番号メモ' })).toHaveAttribute('href', '/tools/train-number');
-        expect(screen.getByRole('link', { name: '入出場' })).toHaveAttribute('href', '#%E5%85%A5%E5%87%BA%E5%A0%B4');
         expect(screen.getByText('東大宮操~回8610M~大宮')).toBeInTheDocument();
-        expect(container.querySelector('#入出場')).not.toBeNull();
-        expect(container.querySelector('#その他-旅客機')).not.toBeNull();
+        expect(container.querySelector('article')).not.toBeNull();
     });
 });
